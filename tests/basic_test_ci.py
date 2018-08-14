e86519762b35291073c58b62547d8643971e9488
moshe <moshe.kaplan@brightaqua.com>
2018-08-14 10:43:09 +0200

    Basic travis CI side, that will wait for a "UP" message from the client to assert it to true

---
+++ tests/basic_test_ci.py
@@ -0,0 +1,16 @@
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(
+    project, subscription_name)
+
+def callback(message):
+    print('Received message: {}'.format(message))
+    message.ack()
+
+subscriber.subscribe(subscription_path, callback=callback)
+
+# The subscriber is non-blocking, so we must keep the main thread from
+# exiting to allow it to process messages in the background.
+print('Listening for messages on {}'.format(subscription_path))
+while True:
+    time.sleep(60)
+
