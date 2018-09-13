b74ad98df0c3a7540446227aeb1c8dde21c37598
moshe <moshe.kaplan@brightaqua.com>
2018-09-13 14:13:45 +0300

    fix

--- tests/basic_test_agent.py
+++ tests/basic_test_agent.py
@@ -11,6 +11,6 @@ project = 'spacemesh-198810'
 topic_name = 'devnet_tests'
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project, topic_name)
-data = 'UP1'
+data = ''
 data = data.encode('utf-8')
 publisher.publish(topic_path, data=data)
