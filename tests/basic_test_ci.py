724ec0d4c25074203b8dfaab8b88aa8f807a7bee
moshe <moshe.kaplan@brightaqua.com>
2018-09-13 14:22:56 +0300

    move to assert equal

--- tests/basic_test_ci.py
+++ tests/basic_test_ci.py
@@ -21,7 +21,7 @@ class Test0(unittest.TestCase):
         print('Listening for messages on {}'.format(self.subscription_path))
         time.sleep(self.testLen)
         print('test: ' + self.message)
-        self.assertEquals('UP', self.message)
+        self.assertEqual('UP', self.message)
 
 if __name__ == '__main__':
     unittest.main()
