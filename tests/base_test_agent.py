504b0228a8a5b3bc993820e56a5963730feb4526
moshe <moshe.kaplan@brightaqua.com>
2018-09-25 17:16:35 +0300

    faster check

--- tests/base_test_agent.py
+++ tests/base_test_agent.py
@@ -15,7 +15,7 @@ class BaseDevnetAgent:
         self.publisher.publish(self.topic_path, data=data)
 
     def get_node_id(self):
-        time.sleep(30)
+        time.sleep(15)
         pattern = re.compile("[.]*NodeID (\w+)")
         node_id = b'NULL'
         for line in open('/opt/logs/node.log', "r", encoding="utf-8"):
