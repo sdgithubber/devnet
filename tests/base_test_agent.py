496d9728261bd05f15af2abc65aa4e9b93b742c9
moshe <moshe.kaplan@brightaqua.com>
2018-09-25 18:12:46 +0300

    try to work w/ unicode

--- tests/base_test_agent.py
+++ tests/base_test_agent.py
@@ -16,11 +16,11 @@ class BaseDevnetAgent:
 
     def get_node_id(self):
         time.sleep(15)
-        pattern = re.compile("[.]*NodeID (\w+)")
+        pattern = re.compile(u'[.]*NodeID (\w+)')
         node_id = 'NULL'
         for line in open('/opt/logs/node.log', "r", encoding="utf-8"):
             results = pattern.match(line)
-            print("results:" + line.decode())
+            #print("results:" + line.encode('utf-8').decode()))
             if results != None:
                 print("results2:" + results.group(1))
                 node_id = results.group(1)
