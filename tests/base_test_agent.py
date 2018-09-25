3ca50c1a4dc24080c15e9d9add446c7dbf7caf44
moshe <moshe.kaplan@brightaqua.com>
2018-09-25 18:17:06 +0300

    fix

--- tests/base_test_agent.py
+++ tests/base_test_agent.py
@@ -21,7 +21,7 @@ class BaseDevnetAgent:
         for line in open('/opt/logs/node.log', "r", encoding="utf-8"):
             results = pattern.match(line)
             #print("results:" + line.encode('utf-8').decode()))
-			print("results:")
+            print("results:")
             if results != None:
                 print("results2:" + results.group(1))
                 node_id = results.group(1)
