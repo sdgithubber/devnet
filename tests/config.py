4b5641d5a0487988eb59a619115433326abc1b3d
moshe <moshe.kaplan@brightaqua.com>
2018-10-04 15:03:14 +0300

    fix

--- tests/config.py
+++ tests/config.py
@@ -1,7 +1,7 @@
 CONFIG = {
     'project': 'spacemesh-198810',
     'topic_name_upstream': 'devnet_tests',
-    'subscription_name_upstream': 'devnet_tests_ci'
-    'topic_name_downstream': 'devnet_tests_downstream'
+    'subscription_name_upstream': 'devnet_tests_ci',
+    'topic_name_downstream': 'devnet_tests_downstream',
     'subscription_name_downstream': 'devnet_tests_agent'
 }
