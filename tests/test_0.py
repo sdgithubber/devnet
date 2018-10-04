aa4c42ef4ce72bca2dc0b295560a13c0aae62e61
moshe <moshe.kaplan@brightaqua.com>
2018-10-04 18:14:56 +0300

    also for test 0

--- tests/test_0.py
+++ tests/test_0.py
@@ -8,10 +8,12 @@ import unittest
 class Test0(BaseTest):
     def test_verifyUp(self):
         self.subscriber_upstream.subscribe(self.subscription_path_upstream, callback=self.callback)
+        self.send('SEND_UP')
         for i in range(0, self.testLen):
             if self.endFlag:
                 break
             time.sleep(1)
+        self.send('END')
 
         self.assertEqual(b'UP', self.message)
 
