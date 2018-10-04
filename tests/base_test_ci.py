5dfd5d61eb24c458052024a715febadba888594e
moshe <moshe.kaplan@brightaqua.com>
2018-10-04 16:09:28 +0300

    Support downstream channel in the CI side

--- tests/base_test_ci.py
+++ tests/base_test_ci.py
@@ -14,10 +14,18 @@ class BaseTest(unittest.TestCase):
         self.subscriber_upstream = pubsub_v1.SubscriberClient()
         self.subscription_path_upstream = self.subscriber_upstream.subscription_path(project, subscription_name_upstream)
 
+        topic_name_downstream = config.CONFIG['topic_name_downstream']
+        self.publisher_downstream = pubsub_v1.PublisherClient()
+        self.topic_path_downstream = self.publisher_downstream.topic_path(project, topic_name_downstream)
+
     def callback(self, message):
         self.message = message.data
         message.ack()
         self.endFlag = True
+
+    def send(self, data):
+        data = data.encode('utf-8')
+        self.publisher_downstream.publish(self.topic_path_downstream, data=data)
 
 if __name__ == '__main__':
     unittest.main()
