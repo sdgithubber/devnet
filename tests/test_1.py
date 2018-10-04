282e905bb189b12ff7cf69d94789f7ed543c002f
moshe <moshe.kaplan@brightaqua.com>
2018-10-04 16:12:02 +0300

    Add messages sendings to test0

--- tests/test_1.py
+++ tests/test_1.py
@@ -8,11 +8,13 @@ import unittest
 class Test1(BaseTest):
     def test_sendId(self):
         self.subscriber_upstream.subscribe(self.subscription_path_upstream, callback=self.callback)
+        self.send('GET_NODE_ID')
         for i in range(0, self.testLen):
             if self.endFlag:
                 print(self.message)
                 break
             time.sleep(1)
+        self.send('END')
 
         self.assertNotEqual(b'NULL', self.message)
         self.assertLess(5, len(self.message))
