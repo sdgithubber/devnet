5c56365bff92fe6c686da051ae338ce7908611c7
moshe <mokplan@gmail.com>
2018-10-18 12:05:23 +0300

    debug

--- tests/base_test_agent.py
+++ tests/base_test_agent.py
@@ -61,10 +61,11 @@ class BaseDevnetAgent:
     def get_node_id(self):
         time.sleep(15)
         node_id = 'NULL'
+        print(1)
         try
             node_id = next(os.walk('/opt/logs'))[1][0]
         except
-            pass
+            print('Error finding log folder')
         return node_id
 
 if __name__ == '__main__':
