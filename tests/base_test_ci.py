f5468799201a81cfbe5299685847c623fc5f6cac
moshe <mokplan@gmail.com>
2018-10-18 10:01:25 +0300

    cleanup logs for spwan

--- tests/base_test_ci.py
+++ tests/base_test_ci.py
@@ -55,15 +55,14 @@ class BaseTest(unittest.TestCase):
                 cmd = 'docker run --rm --network=devnet --name node -p 7513:7513 spacemesh/node:latest /go/src/github.com/spacemeshos/go-spacemesh/go-spacemesh >> /root/spacemesh/devnet/logs/node.log 2>&1'
                 print(cmd)
                 result = shell.spawn(cmd.split(' '))
-                print('Node started: ' + "".join(map(chr, result.output)))
+                print('Node started')
                 cmd = 'docker run --rm --network=devnet --name agent -p 8080:8080 -v /root/spacemesh/devnet/tests:/opt/devnet -v /root/spacemesh/devnet/logs:/opt/logs spacemesh/devnet_agent:latest python3 /opt/devnet/base_test_agent.py >> /root/spacemesh/devnet/logs/test.log 2>&1'
                 print(cmd)
                 result = shell.spawn(cmd.split(' '))
-                print('Agent started: ' + "".join(map(chr, result.output)))
+                print('Agent started')
             except Exception as e:
                 print('Node/Agent started failed')
                 print(e.__doc__ )
-                print(e.message)
 
 if __name__ == '__main__':
     unittest.main()
