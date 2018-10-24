413486fe3b8b6380819d8e8475dc0916c5c64518
moshe <mokplan@gmail.com>
2018-10-24 10:20:15 +0300

    Initial Test2: inject 3 pairs and see all works

---
+++ tests/test_2.py
@@ -0,0 +1,21 @@
+from base_test_ci import BaseTest
+import config
+import os
+import time
+from google.cloud import pubsub_v1
+import unittest
+
+class Test2(BaseTest):
+    def test_sendId(self):
+        seeds = []
+        for i in range(0, 3):
+            self.start_node_agent_pair()
+            self.send_and_wait('GET_NODE_ID')
+            self.assertNotEqual(b'NULL', self.message)
+            self.assertLess(5, len(self.message))
+            seeds.append(self.message)
+
+        self.assertEqual(3, len(seeds))
+
+if __name__ == '__main__':
+    unittest.main()
