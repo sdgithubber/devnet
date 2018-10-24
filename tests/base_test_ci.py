24656992ae85dcdd08edd0d627609e32e1cac232
moshe <mokplan@gmail.com>
2018-10-24 10:55:37 +0300

    Cleanup agents and nodes before starting to run

--- tests/base_test_ci.py
+++ tests/base_test_ci.py
@@ -63,8 +63,29 @@ class BaseTest(unittest.TestCase):
         except Exception as e:
             print('Docker start failed')
             print(e.__doc__ )
-        
+
+    def run_cmd_interactive(self, cmd):
+        try:
+            shell = spur.SshShell(
+                hostname=config.CONFIG['host'], 
+                username=config.CONFIG['host_user'], 
+                password=config.CONFIG['host_password'], 
+                missing_host_key=spur.ssh.MissingHostKey.accept
+            )
+            with shell:
+                result = shell.run(cmd.split(' '))
+                print('Run interactive: ' + cmd)
+        except Exception as e:
+            print('Run interactive failed: ' + cmd)
+            print(e.__doc__ )
+
+    def stop_docker(self, name):
+        self.run_cmd_interactive("docker stop " + name)
+        self.run_cmd_interactive("docker rm " + name)
+
     def start_node_agent_pair(self):
+        self.stop_docker('node_' + str(self.agents))
+        self.stop_docker('agnet_' + str(self.agents))
         self.start_docker('docker run --network=devnet --name node_' + str(self.agents) + ' -p ' + str(7513 + self.agents) + ':7513 -v /root/spacemesh/devnet/logs:/root/.spacemesh/nodes/ spacemesh/node:latest /go/src/github.com/spacemeshos/go-spacemesh/go-spacemesh')
         self.start_docker('docker run --network=devnet --name agent_' + str(self.agents) + ' -v /root/spacemesh/devnet/tests:/opt/devnet -v /root/spacemesh/devnet/logs:/opt/logs -e SUBSCRIPTION_NAME_DOWNSTREAM=devnet_tests_agent_' + str(self.agents) + ' -e NODE=node_' + str(self.agents) + ' spacemesh/devnet_agent:latest python3 /opt/devnet/base_test_agent.py')
         self.agents += 1
