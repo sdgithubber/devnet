632f1e6ca66d5906fb75614f56ada0672b48db4c
moshe <mokplan@gmail.com>
2018-10-26 00:47:20 +0300

    typo

--- tests/base_test_ci.py
+++ tests/base_test_ci.py
@@ -36,7 +36,8 @@ class BaseTest(unittest.TestCase):
         message.ack()
 
     def create_phase(self, test, phase):
-        return (self.phase = 'test_' + str(test) + '_phase_' + str(phase) + '_' + str(calendar.timegm(time.gmtime())))
+        self.phase = 'test_' + str(test) + '_phase_' + str(phase) + '_' + str(calendar.timegm(time.gmtime()))
+        return self.phase
 
     def send(self, data, phase=self.phase):
         print(data)
