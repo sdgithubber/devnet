468e665c67cf8fef21a452aba9ef0ed9e5fe8d4f
moshe <mokplan@gmail.com>
2018-10-29 15:48:08 +0200

    some debugging

--- tests/base_test_agent.py
+++ tests/base_test_agent.py
@@ -67,6 +67,7 @@ class BaseDevnetAgent:
     def send(self, data):
         data = data.encode('utf-8')
         self.publisher_upstream.publish(self.topic_path_upstream, data=data, phase=self.phase)
+        logging.INFO("sent " + data)
 
     def get_node_id(self):
         time.sleep(15)
