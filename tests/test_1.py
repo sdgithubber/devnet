fc17f79fe22ae0ef5ffd5b86ec71b49effee4d5c
moshe <mokplan@gmail.com>
2018-11-06 18:26:25 +0200

    1. Align test_1 2. Change phase 0 of test 2 to non bootstrap

--- tests/test_1.py
+++ tests/test_1.py
@@ -7,11 +7,14 @@ import unittest
 
 class Test1(BaseTest):
     def test_sendId(self):
-        self.start_node_agent_pair()
-        self.send_and_wait('GET_NODE_ID')
+        phase_0 = self.create_phase("1")
+        self.start_node_agent_pair(bootstrap='false')
+        self.send('GET_NODE_ID')
+        self.wait_for_response(1)
 
-        self.assertNotEqual(b'NULL', self.message)
-        self.assertLess(5, len(self.message))
+        self.assertEqual(1, len(self.messages))
+        self.assertNotEqual(b'NULL', self.messages[0])
+        self.assertLess(15, len(self.messages[0]))
 
 if __name__ == '__main__':
     unittest.main()
