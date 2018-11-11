3124b96d4c1d5746448832da3dbdcad948411477
moshe <mokplan@gmail.com>
2018-11-11 12:19:24 +0200

    debug

--- tests/base_test_ci.py
+++ tests/base_test_ci.py
@@ -59,6 +59,7 @@ class BaseTest(unittest.TestCase):
                 logging.info(self.messages)
                 break
             time.sleep(1)
+        time.sleep(10)
 
     def send_and_wait(self, data):
         self.send(data)
