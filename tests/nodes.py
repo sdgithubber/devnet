261ee3f97620a3be03826206987d145a18a4b801
moshe <mokplan@gmail.com>
2018-11-11 17:43:13 +0200

    encapsulate (2)

---
+++ tests/nodes.py
@@ -0,0 +1,24 @@
+import logging
+from logging import Logger
+
+class Nodes():
+    def __init__(self):
+        self.nodes = []
+
+    def add(self, node):
+        self.nodes.append(node)
+
+    def __iadd__(self, l):
+        self.nodes += l
+        return self    
+
+    def get_nodes(self, num):
+        tmp = self.nodes
+        selected_nodes = []
+        for i in range(0, num):
+            if len(tmp) == 0:
+                 break
+            select = random.randint(0, len(tmp))
+            selected_nodes.append(tmp[select])
+            tmp.remove(select)
+        return selected_nodes
