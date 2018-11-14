d6b203635040857d22553346b341fd7e3419c7a6
moshe <mokplan@gmail.com>
2018-11-15 01:49:22 +0200

    fit test 0 + 1 to new method

--- tests/test_1.py
+++ tests/test_1.py
@@ -7,7 +7,7 @@ import unittest
 
 class Test1(BaseTest):
     def test_sendId(self):
-        messages = self.run_phase(nodes = 1, bootstrap = 'false', message = 'GET_NODE_ID')
+        messages = self.run_phase(nodes = 1, bootstrap = 'false')
         self.assertLess(15, len(messages[0]))
 
 if __name__ == '__main__':
