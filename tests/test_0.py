fdd21e268f726e5a7ee3f8b66156b62f83357eaf
moshe <mokplan@gmail.com>
2018-11-15 01:54:11 +0200

    typo

--- tests/test_0.py
+++ tests/test_0.py
@@ -9,7 +9,7 @@ class Test0(BaseTest):
     def test_verifyUp(self):
         testers_nodes = 1
         messages = self.run_phase(nodes = testers_nodes, bootstrap = 'false')
-        messages = self.send_and_wait('GET_DHT_SIZE', testers_nodes)
+        messages = self.send_and_wait('SEND_UP', testers_nodes)
         self.assertEqual('UP', messages[0])
 
 if __name__ == '__main__':
