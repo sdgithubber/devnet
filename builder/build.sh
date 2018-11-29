01734554e8f2798103ca5d7ec29940016d85eecf
Elad Gavra <gavrad@gmail.com>
2018-11-29 00:44:58 +0000

    Push to google registry

--- builder/build.sh
+++ builder/build.sh
@@ -16,8 +16,15 @@ go build
 cp $IMAGES_DIR/Dockerfile $MY_TMP
 cp go-spacemesh $MY_TMP
 ls -l $MY_TMP
+
 BUILD_CMD="sudo docker build -t node:$COMMIT $MY_TMP"
 $BUILD_CMD
+
+TAG_CMD="sudo docker tag node:$COMMIT gcr.io/spacemesh-198810/node:$COMMIT"
+$TAG_CMD
+
+PUSH_CMD="sudo docker push gcr.io/spacemesh-198810/node:$COMMIT"
+$PUSH_CMD
 
 # cleanup
 popd
